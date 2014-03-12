e293ea6f4f6f910c9fa64928e3188feba36c9c2d
DAW <daw@daw-VirtualBox.(none)>
2014-03-12 17:41:10 +0100

    Ejercicio final Javascript. Formulario y errores

--- JavaScript/ej09-final/js/main.js
+++ JavaScript/ej09-final/js/main.js
@@ -1,29 +1,14 @@
-window.$ = Element.prototype.$ = function(selector){
-    var that = (this instanceof Element) ? this : document;
-    var elems = that.querySelectorAll(selector);//esta linea se puede cambiar por '$'
-
-    return (elems.length === 1) ? elems[0] : elems;
+window.$=Element.prototype.$=function(selector){
+    var that=(this instanceof Element) ? this : document;
+    var elems = that.querySelectorAll(selector);
+    return (elems.length===1) ? elems[0] : elems;
 };
 
-var anade = (function(){
-    "use strict";
-
-    var fecha = /(0[1-9]|[1-2][1-9]|30|31)\/(0[1-9]|1[0-2])\/(\d{4})/;
-
-    console.log(fecha.test("Naci en Oñati el 24/05/1985"));
-    console.log(fecha.test("Naci en Oñati el 30/02/1982"));
-    console.log(fecha.test("Naci en Oñati el 30/13/1982"));
-    console.log(fecha.test("Naci en Oñati el 99/99/1985"));
-    console.log(fecha.test("Naci en Oñati el 00/99/1985"));
-
-    console.log("Naci en Oñati el 24/05/1985".match(fecha));
-
-    var email = /^(\w+)((\.|-)(\w+))*@(\w+)(\.\w{2,})+$/;
-
-    console.log(email.test("anderidoyaga@gmail.com"));
-    console.log(email.test("anderidoyaga@gmail.com.es"));
-    console.log(email.test("anderidoyaga@gmail.z"));//error
-    console.log(email.test(".ander@gmail.com"));//error
-    console.log(email.test("a@a.com"));
-
-})();
+window.onload = function(){
+    var formulario = $("#registro");
+    var textos=["Rellene el campo",
+                "El password debe tener una longitud mínima de 6 caracteres, y contener al menos una letra minúscula, una letra mayúscula y un dígito.",
+                "Esto no es un email",
+                "El texto introducido en el campo de comentarios no debe exceder los 50 caracteres."];
+    formulario.validar(textos);
+};
---
+++ JavaScript/ej09-final/js/validadorFormulario.js
@@ -0,0 +1,85 @@
+window.$=Element.prototype.$=function(selector){
+    var that=(this instanceof Element) ? this : document;
+    var elems = that.querySelectorAll(selector);
+    return (elems.length===1) ? elems[0] : elems;
+};
+
+HTMLFormElement.prototype.validar = function(textos){
+    'use strict';
+    var leidos=["Rellene el campo",
+                "El password debe tener una longitud mínima de 6 caracteres, y contener al menos una letra minúscula, una letra mayúscula y un dígito.",
+                "Esto no es un email",
+                "El texto introducido en el campo de comentarios no debe exceder los 50 caracteres."];
+    if(!(textos === undefined || textos === null || textos.length === 0))leidos=textos;
+
+    var form = this;
+    var input = this.$('input,textarea');
+
+
+    var validarFormulario=function(e){
+        var bien=true;
+        for (var i = input.length - 1; i >= 0; i--) {
+            var tipo = input[i].dataset.validator;
+            switch(tipo) {
+                case 'required' : bien=validador.requerido(input[i].value);break;
+                case 'password' : bien=validador.password(input[i].value);break;
+                case 'minimo' : bien=validador.minimo(input[i].value);break;
+                case 'email' : bien=validador.email(input[i].value);break;
+            }
+            if(!bien){
+                e.preventDefault();
+            }
+        }
+    };
+
+    var errorMessage=function(textError, campo){
+        var item = document.createElement('div');
+        item.innerText = textError;
+        campo.parentNode.insertBefore(item, campo.nextSibling);
+        //form.appendChild(item);
+    };
+
+    /*var deleteMessage = function(campo){
+        campo.parentNode.removeChild(item);
+    };*/
+
+    var required = function(e){
+        if(!validador.requerido(this.value)){
+            console.log(leidos[0]);
+            errorMessage(leidos[0], this);
+        }
+    };
+    var password = function(e){
+        if(!validador.password(this.value)){
+            errorMessage(leidos[1], this);
+            console.log(leidos[1]);
+        }
+    };
+    var minimo = function(e){
+        if(!validador.minimo(this.value)){
+            errorMessage(leidos[3], this);
+            console.log(leidos[3]);
+        }
+    };
+    var email = function(e){
+        if(!validador.email(this.value)){
+            errorMessage(leidos[2], this);
+            console.log(leidos[2]);
+        }
+    };
+
+
+    for (var i = input.length - 1; i >= 0; i--) {
+        var tipo = input[i].dataset.validator;
+        switch(tipo) {
+            case 'required' : input[i].addEventListener('blur', required);break;
+            case 'password' : input[i].addEventListener('blur', password);break;
+            case 'minimo' : input[i].addEventListener('blur', minimo);break;
+            case 'email' : input[i].addEventListener('blur', email);break;
+        }
+    }
+
+    form.addEventListener('submit', validarFormulario);
+
+};
+
