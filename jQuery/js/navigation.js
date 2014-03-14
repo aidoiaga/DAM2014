d8df67682071b558fc419713e998c188d4f37d30
DAW <daw@daw-VirtualBox.(none)>
2014-03-14 19:31:11 +0100

    Ejercicios animacion jQuery, capitulo 6

---
+++ jQuery/js/navigation.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('#nav li').hover(function(){
+        $(this).addClass('hover').find('ul').show();
+    }, function(){
+        $(this).removeClass('hover').find('ul').hide();
+    });
+});
