925435f3f4ad9944cae43336a45310a63695fe3c
DAW <daw@daw-VirtualBox.(none)>
2014-03-19 19:06:38 +0100

    Ejercicio Validacion de Formularios jQuery

--- jQuery-Validator/js/validador.js
+++ jQuery-Validator/js/validador.js
@@ -10,6 +10,7 @@
                 'password' : 'El password debe tener una longitud mínima de 6 caracteres, y contener al menos una letra minúscula, una letra mayúscula y un dígito.',
                 'email' : 'Esto no es un email',
                 'minimo' : 'El texto introducido en el campo de comentarios no debe exceder los 50 caracteres.'};
+           // if(!(textos === undefined || textos === null || textos.length === 0))leidos=textos;
 
             var input = $this.find('input,textarea');
 
