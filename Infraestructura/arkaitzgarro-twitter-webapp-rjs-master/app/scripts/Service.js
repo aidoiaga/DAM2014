8b41cad647988ecb8cc840453375e4037bcfc517
Ander Idoiaga <anderidoyaga@gmail.com>
2014-04-22 18:31:36 +0200

    Modulo Service para conexion ajax a twitter

---
+++ Infraestructura/arkaitzgarro-twitter-webapp-rjs-master/app/scripts/Service.js
@@ -0,0 +1,18 @@
+define('Service', ['jquery'], function($){
+    "use strict";
+
+    console.log('Service module started');
+
+    var getTweets = function(config, success, error){
+        $.ajax({
+            url : '/app/data/tweets.json',
+            dataType : 'JSON',
+            success : success,
+            error : error
+        });
+    };
+
+    return{
+        getTweets : getTweets
+    };
+});
